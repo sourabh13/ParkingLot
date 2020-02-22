d75b7060f2bdabc8047385a934aea9062dd55cbc
Sourabh <sourbans@amazon.com>
2020-02-22 10:54:04 +0530

    Create LargeParkingSpot which is type of ParkingSpot

---
+++ src/main/java/parkinglot/ParkingSpot/LargeParkingSpot.java
@@ -0,0 +1,11 @@
+package parkinglot.ParkingSpot;
+
+import parkinglot.Enums.ParkingSpotType;
+
+public class LargeParkingSpot extends ParkingSpot {
+
+    public LargeParkingSpot(String number) {
+        super(ParkingSpotType.LARGE, number);
+    }
+
+}

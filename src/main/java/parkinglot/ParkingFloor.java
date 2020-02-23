21843948d37344b8dd325fc23e411f8b35523237
Sourabh <sourbans@amazon.com>
2020-02-23 09:14:06 +0530

    Added features for getting all the slot numbers and registration numbers for a vehicle colour, slot number for a vehicle parked

--- src/main/java/parkinglot/ParkingFloor.java
+++ src/main/java/parkinglot/ParkingFloor.java
@@ -3,6 +3,7 @@ package parkinglot;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,12 +17,15 @@ public class ParkingFloor {
 
     private int id;
 
+    private int maxSize;
+
     private HashMap<Integer, ParkingSpot> largeParkingSpots;
 
     private TreeSet<Integer> availableLargeParkingSpots;
 
-    public ParkingFloor(int id) {
+    public ParkingFloor(int id, int maxSize) {
         this.id = id;
+        this.maxSize = maxSize;
         largeParkingSpots = new HashMap<>();
         availableLargeParkingSpots = new TreeSet<>();
     }
@@ -52,7 +56,9 @@ public class ParkingFloor {
         for (ParkingSpotType parkingSpotType : possibleParkingSpotTypes) {
             availableParkingSpot = getAvailableParkingSpot(parkingSpotType);
             if(availableParkingSpot != null) {
-                assignVehicleToParkingSpot(vehicle, availableParkingSpot);
+                assignVehicleToParkingSpot(vehicle, availableParkingSpot,
+                        getSetOfAvailableParkingSpots(parkingSpotType),
+                        getParkingSpots(parkingSpotType));
             }
         }
         return availableParkingSpot;
@@ -61,20 +67,77 @@ public class ParkingFloor {
     public ParkingSpot leave(Integer number, ParkingSpotType parkingSpotType) {
 
         if(isValidParkingSpotType(parkingSpotType)) {
-            switch (parkingSpotType) {
-                case LARGE:
-                    return freeParkingSpot(number, availableLargeParkingSpots, largeParkingSpots);
-            }
+            return freeParkingSpot(number, getSetOfAvailableParkingSpots(parkingSpotType),
+                    getParkingSpots(parkingSpotType));
         }
         System.out.println("Invalid parking spot type provided.");
         return null;
+    }
+
+    private TreeSet<Integer> getSetOfAvailableParkingSpots(ParkingSpotType parkingSpotType) {
+
+        if(isValidParkingSpotType(parkingSpotType)) {
+            switch (parkingSpotType) {
+                case LARGE:
+                    return availableLargeParkingSpots;
+            }
+        }
+        return new TreeSet<>();
+    }
+
+    private HashMap<Integer, ParkingSpot> getParkingSpots(ParkingSpotType parkingSpotType) {
+
+        if(isValidParkingSpotType(parkingSpotType)) {
+            switch (parkingSpotType) {
+                case LARGE:
+                    return largeParkingSpots;
+            }
+        }
+        return new HashMap<>();
+    }
+
+    public List<String> getAllRegNumbersOfColour(String colour) {
+
+        List<ParkingSpot> parkingSpots =  new ArrayList<>(largeParkingSpots.values());
+
+        return parkingSpots
+                .stream()
+                .filter(parkingSpot -> parkingSpot.getVehicle().getColour().equals(colour))
+                .map(parkingSpot -> parkingSpot.getVehicle().getRegNumber())
+                .collect(Collectors.toList());
+    }
+
+    public Integer getSlotNumberForVehicle(String regNumber) {
+
+        List<ParkingSpot> parkingSpots =  new ArrayList<>(largeParkingSpots.values());
+
+        Optional<ParkingSpot> requiredParkingSpot = parkingSpots.stream()
+                .filter(parkingSpot -> parkingSpot.getVehicle().getRegNumber().equals(regNumber))
+                .findAny();
+
+        if(!requiredParkingSpot.isPresent()) {
+            System.out.println("Vehicle with registration number {" + regNumber + "} not present.");
+            return null;
+        }
+        return requiredParkingSpot.get().getNumber();
+    }
+
+    public List<Integer> getAllSlotNumbersOfColor(String colour) {
+
+        List<ParkingSpot> parkingSpots =  new ArrayList<>(largeParkingSpots.values());
+
+        return parkingSpots
+                .stream()
+                .filter(parkingSpot -> parkingSpot.getVehicle().getColour().equals(colour))
+                .map(ParkingSpot::getNumber)
+                .collect(Collectors.toList());
     }
 
     private ParkingSpot freeParkingSpot(Integer number, TreeSet<Integer> availableParkingSpots,
             HashMap<Integer, ParkingSpot> parkingSpots) {
 
         if(!parkingSpots.containsKey(number)) {
-            System.out.println("The parking spot " + number + " is already free.");
+            System.out.println("The parking spot {" + number + "} is already free.");
             return null;
         }
         ParkingSpot parkingSpot = parkingSpots.get(number);
@@ -83,21 +146,12 @@ public class ParkingFloor {
         return parkingSpot;
     }
 
-    private void assignVehicleToParkingSpot(Vehicle vehicle, ParkingSpot parkingSpot) {
-        switch (parkingSpot.getType()) {
-            case LARGE:
-                assignVehicleToParkingSpot(vehicle, parkingSpot, availableLargeParkingSpots, largeParkingSpots);
-                break;
-        }
-    }
-
     private void assignVehicleToParkingSpot(Vehicle vehicle, ParkingSpot parkingSpot,
             TreeSet<Integer> availableParkingSpots,
             HashMap<Integer, ParkingSpot> parkingSpots) {
 
         availableParkingSpots.remove(parkingSpot.getNumber());
         parkingSpots.get(parkingSpot.getNumber()).assignVehicle(vehicle);
-
     }
 
     private Boolean isValidVehicleType(VehicleType vehicleType) {
--- src/test/java/parkinglot/ParkingFloorTest.java
+++ src/test/java/parkinglot/ParkingFloorTest.java
@@ -1,7 +1,11 @@
 package parkinglot;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.Test;
 import parkinglot.Enums.ParkingSpotType;
@@ -12,12 +16,10 @@ import parkinglot.Vehicles.Vehicle;
 
 public class ParkingFloorTest {
 
-
-
     @Test
     public void addParkingSpotTest() {
 
-        ParkingFloor parkingFloor = new ParkingFloor(1);
+        ParkingFloor parkingFloor = new ParkingFloor(1,1);
         ParkingSpot parkingSpot = new LargeParkingSpot(1);
         Boolean successful = parkingFloor.addParkingSpot(parkingSpot);
         Boolean expected = Boolean.TRUE;
@@ -27,7 +29,7 @@ public class ParkingFloorTest {
     @Test
     public void parkVehicleTest() {
 
-        ParkingFloor parkingFloor = new ParkingFloor(1);
+        ParkingFloor parkingFloor = new ParkingFloor(1, 1);
         ParkingSpot parkingSpot = new LargeParkingSpot(1);
         Vehicle vehicle = new Car("ABCD-EFGH", "White");
         parkingFloor.addParkingSpot(parkingSpot);
@@ -39,7 +41,7 @@ public class ParkingFloorTest {
     @Test
     public void leaveParkingTest() {
 
-        ParkingFloor parkingFloor = new ParkingFloor(1);
+        ParkingFloor parkingFloor = new ParkingFloor(1, 1);
         ParkingSpot parkingSpot = new LargeParkingSpot(1);
         Vehicle vehicle = new Car("ABCD-EFGH", "White");
         parkingFloor.addParkingSpot(parkingSpot);
@@ -48,5 +50,71 @@ public class ParkingFloorTest {
         assertEquals(parkingSpot.getNumber(), actualParkingSpot.getNumber());
         assertEquals(ParkingSpotType.LARGE, parkingSpot.getType());
     }
+
+    @Test
+    public void getAllRegNumbersOfColourTest() {
+
+        ParkingFloor parkingFloor = createSampleParkingFloorFourSize();
+
+        List<String> allRegsOfColor = parkingFloor.getAllRegNumbersOfColour("White");
+
+        assertEquals(3, allRegsOfColor.size());
+        assertTrue(allRegsOfColor.contains("ABCD-EFGH"));
+        assertTrue(allRegsOfColor.contains("IJKL-MNOP"));
+        assertTrue(allRegsOfColor.contains("IJKL-EFGH"));
+        assertTrue(!allRegsOfColor.contains("ABCD-MNOP"));
+    }
+
+    @Test
+    public void getAllSlotsOfColorTest() {
+
+        ParkingFloor parkingFloor = createSampleParkingFloorFourSize();
+
+        List<Integer> allSlotNumbersOfColor = parkingFloor.getAllSlotNumbersOfColor("White");
+
+        assertEquals(3, allSlotNumbersOfColor.size());
+        assertTrue(allSlotNumbersOfColor.contains(1));
+        assertTrue(allSlotNumbersOfColor.contains(2));
+        assertTrue(allSlotNumbersOfColor.contains(4));
+        assertTrue(!allSlotNumbersOfColor.contains(3));
+
+    }
+
+    @Test
+    public void getSlotNumberForVehiclePresentTest() {
+
+        ParkingFloor parkingFloor = createSampleParkingFloorFourSize();
+        Integer slotNumber = parkingFloor.getSlotNumberForVehicle("ABCD-MNOP");
+
+        assertNotNull(slotNumber);
+        assertEquals(3, slotNumber.intValue());
+    }
+
+    @Test
+    public void getSlotNumberForVehicleAbsentTest() {
+
+        ParkingFloor parkingFloor = createSampleParkingFloorFourSize();
+        Integer slotNumber = parkingFloor.getSlotNumberForVehicle("XXXX-XXXX");
+
+        assertNull(slotNumber);
+    }
+
+
+    private ParkingFloor createSampleParkingFloorFourSize() {
+
+        ParkingFloor parkingFloor = new ParkingFloor(1, 4);
+        parkingFloor.addParkingSpot(new LargeParkingSpot(1));
+        parkingFloor.addParkingSpot(new LargeParkingSpot(2));
+        parkingFloor.addParkingSpot(new LargeParkingSpot(3));
+        parkingFloor.addParkingSpot(new LargeParkingSpot(4));
+
+        parkingFloor.parkVehicle(new Car("ABCD-EFGH", "White"));
+        parkingFloor.parkVehicle(new Car("IJKL-MNOP", "White"));
+        parkingFloor.parkVehicle(new Car("ABCD-MNOP", "Red"));
+        parkingFloor.parkVehicle(new Car("IJKL-EFGH", "White"));
+
+        return parkingFloor;
+    }
+
 
 }
